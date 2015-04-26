4268c68064dd774e7477539f95409e4b941f3e83
ANUJ DESHMUKH <anuj.deshmukh@student.csulb.edu>
2015-04-26 13:11:45 -0700

    InitialCommit
    Master detailed native project created.
    Without working on anything.

---
+++ ProlificLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProlificLibrary
+//
+//  Created by ANUJ DESHMUKH on 4/26/15.
+//  Copyright (c) 2015 DESHMUKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProlificLibrary/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ProlificLibrary
+//
+//  Created by ANUJ DESHMUKH on 4/26/15.
+//  Copyright (c) 2015 DESHMUKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
